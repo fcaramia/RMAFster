9868afc360bb35b64b69175258ec51e0ae20e1fb
fcaramia <fcaramia@gmail.com>
2020-04-19 23:13:30 +1000

    Retracted, do nothing onLoad

--- R/zzz.R
+++ R/zzz.R
@@ -1,7 +1,3 @@
 .onLoad <- function(libname, pkgname){
 
-  py_avail = reticulate::py_available()
-  if(py_avail==F){
-    stop("Python not found, RMAFster uses python3, you can install a python environment with install_miniconda(), or install python in your system")
-  }
 }
