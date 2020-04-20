b8f2dd8c011cf8993cb1aca8c42e36cec94f2402
fcaramia <fcaramia@gmail.com>
2020-04-20 10:27:38 +1000

    warning onLoad always

--- R/zzz.R
+++ R/zzz.R
@@ -1,11 +1,7 @@
 .onLoad <- function(libname, pkgname){
-
-  avail = reticulate::py_available()
-  if(avail == FALSE){
-    warning("RMAFster uses a local python environment to execute RmafsterCalc,
-          unless you specify a python environment using reticule::use...
-          you will be prompted to install miniconda. Select (Y) to proceed.
-          Python dependencies will be handled automatically")
-  }
+  warning("RMAFster uses a local python environment to execute RmafsterCalc,
+  unless you specify a python environment using reticule::use_...
+  you will be prompted to install miniconda the first time you use RmasterCalc.
+  Select (Y) to proceed. Python dependencies will be handled automatically")
 
 }
