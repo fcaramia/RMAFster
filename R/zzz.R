f2d6c45d28748cf72443b115cbf2101a1e5db94b
fcaramia <fcaramia@gmail.com>
2020-04-20 10:01:07 +1000

    added warning re miniconda and python

--- R/zzz.R
+++ R/zzz.R
@@ -1,7 +1,10 @@
 .onLoad <- function(libname, pkgname){
 
-  warning("RMAFster uses a local python environment to execute RmafsterCalc,
+  if(!reticulate::py_available()){
+    warning("RMAFster uses a local python environment to execute RmafsterCalc,
           unless you specify a python environment using reticule::use...
           you will be prompted to install miniconda. Select (Y) to proceed.
           Python dependencies will be handled automatically")
+  }
+
 }
